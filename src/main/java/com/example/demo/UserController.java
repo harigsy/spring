4e81f9414680dc41c0b215668ea4d243330d5788
harigsy <harigsy2003@gmail.com>
2024-08-09 14:28:20 +0530

    updated UC

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
-public class HomeController {
+public class HelloController {
 
     @GetMapping("/")
-    public String demo() {
+    public String home() {
         return "Greetings from Spring Boot!";
     }
 
