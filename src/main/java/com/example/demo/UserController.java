04ad11b51a58a3b06cffe8be625034e84063637d
harigsy <harigsy2003@gmail.com>
2024-08-09 12:01:22 +0530

    new usercontroller updated

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,14 +1,12 @@
-package com.example.demo;
-
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
-
 @RestController
 public class UserController {
-    @GetMapping
-    public String demo() {
-        return "Hello World";
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
     }
+
 }
